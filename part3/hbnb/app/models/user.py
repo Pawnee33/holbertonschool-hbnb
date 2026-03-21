3252909998c32a042342a9c3ec233287dc55b9dc
Pawnee <pawneedefize@hotmail.com>
2026-03-21 09:47:12 +0000

    fix(user): re-adds first_name and last_name

--- part3/hbnb/app/models/user.py
+++ part3/hbnb/app/models/user.py
@@ -24,6 +24,8 @@ class User(BaseModel):
     """
     __tablename__ = 'users'
 
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(120), nullable=False, unique=True)
     password = db.Column(db.String(128), nullable=False)
     is_admin = db.Column(db.Boolean, default=False, nullable=False)
