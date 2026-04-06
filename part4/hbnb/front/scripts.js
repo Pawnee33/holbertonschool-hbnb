a1c789a0db5b191491dd9d58a5b3753433d3b71f
Pawnee <pawneedefize@hotmail.com>
2026-04-06 13:51:00 +0200

    task 1 completed

--- part4/hbnb/front/scripts.js
+++ part4/hbnb/front/scripts.js
@@ -4,5 +4,39 @@
 */
 
 document.addEventListener('DOMContentLoaded', () => {
-    /* DO SOMETHING */
-  });
+    const loginForm = document.getElementById('login-form');
+
+    if (loginForm) {
+        loginForm.addEventListener('submit', async (event) => {
+            event.preventDefault();
+            // Your code to handle form submission
+            const email = loginForm.elements.email.value
+            const password = loginForm.elements.password.value
+            loginUser(email, password);
+        });
+    }
+});
+
+async function loginUser(email, password) {
+  try {
+    const response = await fetch('http://127.0.0.1:5000/api/v1/auth/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email, password })
+    });
+    // Handle the response
+    if (response.ok) {
+      const data = await response.json();
+      document.cookie = `token=${data.access_token}; path=/`;
+      window.location.href = 'index.html';
+      console.log("Success:", data);
+    } else {
+      alert('Login failed: ' + response.statusText);
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
